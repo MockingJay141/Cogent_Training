9f3c7c3099a42e8fb50fad3fb8143bb7388c2de1
Omkar_Kulkarni <omkar14199@gmail.com>
2024-05-15 20:35:21 -0500

    Week4 Day3(05/15/2024): Continuation of JDBC, Introduction to Java Servlet.

--- src/com/JDBC/Controller/EmployeeController.java
+++ src/com/JDBC/Controller/EmployeeController.java
@@ -10,9 +10,32 @@ import java.util.List;
 
 public class EmployeeController {
 
+    EmployeeService employeeService = null;
+
     public static void main(String[] args) throws SQLException {
 
-        EmployeeService employeeService = new EmployeeService();
+        EmployeeController ec = new EmployeeController();
+        ec.fetchData();
+        System.out.println("========================================");
+        ec.insertData( new Employee(101, "OmkarKulkarni", "omkar@gmail.com", 6000.5F));
+        ec.fetchData();
+        System.out.println("========================================");
+        Employee e = ec.fetchById(101);
+        System.out.println(e);
+        System.out.println("========================================");
+        ec.update(new Employee(101, "Omkar", "omkar@gmail.com", 6000.5F));
+        Employee e1 = ec.fetchById(101);
+        System.out.println(e1);
+        System.out.println("========================================");
+        ec.delete(101);
+        ec.fetchData();
+
+        ec.closeConnection();
+    }
+
+    public void fetchData() throws SQLException {
+
+        employeeService = new EmployeeService();
         List<Employee> employeeList = employeeService.fetchAll();
 
         Iterator<Employee> iterator = employeeList.iterator();
@@ -25,6 +48,56 @@ public class EmployeeController {
                     ", Salary: " + e.getSalary());
         }
 
+
+    }
+
+    public void insertData(Employee employee) throws SQLException {
+
+        employeeService = new EmployeeService();
+        boolean checkInsert =  employeeService.save(employee);
+
+        if (checkInsert){
+            System.out.println("Employee Inserted Successfully with Id: " +employee.getId());
+        }
+        else {
+            System.out.println("Employee insert failed");
+        }
+
+    }
+
+    public Employee fetchById(int id) throws SQLException {
+        employeeService = new EmployeeService();
+        Employee employee = employeeService.fetchById(id);
+
+
+        return employee;
+    }
+
+    public void update(Employee employee) throws SQLException {
+        employeeService = new EmployeeService();
+
+        boolean checkUpdate = employeeService.updateName(employee);
+        if (checkUpdate){
+            System.out.println("Employee Updated Successfully with Id: " + employee.getId());
+        }
+        else {
+            System.out.println("Employee update failed");
+        }
+
+    }
+
+    public void delete(int id) throws SQLException {
+        employeeService = new EmployeeService();
+        boolean checkDelete = employeeService.delete(101);
+        if (checkDelete){
+            System.out.println("Employee Deleted Successfully with Id: " + id);
+        }
+        else {
+            System.out.println("Employee delete failed");
+        }
+    }
+
+    public void closeConnection() throws SQLException {
         DBRepository.closeConnection();
     }
 }
--- src/com/JDBC/Service/EmployeeService.java
+++ src/com/JDBC/Service/EmployeeService.java
@@ -31,4 +31,68 @@ public class EmployeeService {
 
         return employeeList;
     }
+
+    public Employee fetchById(int id) throws SQLException {
+        Connection connection = DBRepository.getConnection();
+        Employee e = null;
+
+        PreparedStatement ps = connection.prepareStatement("SELECT * FROM employee WHERE id = ?");
+        ps.setInt(1, id);
+        ResultSet rs = ps.executeQuery();
+        while (rs.next()){
+
+            e = new Employee(rs.getInt(1), rs.getString(2),
+                    rs.getString(3), rs.getFloat(4));
+        }
+
+        return e;
+    }
+
+    public boolean updateName(Employee employee) throws SQLException {
+        Connection connection = DBRepository.getConnection();
+
+        PreparedStatement ps = connection.prepareStatement("UPDATE employee SET name = ? WHERE id = ?");
+        ps.setString(1, employee.getName());
+        ps.setInt(2, employee.getId());
+        int x = ps.executeUpdate();
+        if (x > 0){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public boolean delete(int id) throws SQLException {
+        Connection connection = DBRepository.getConnection();
+
+        PreparedStatement ps = connection.prepareStatement("DELETE FROM employee WHERE id = ?");
+        ps.setInt(1, id);
+        int x = ps.executeUpdate();
+        if (x > 0){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public boolean save(Employee employee) throws SQLException {
+
+        Connection connection = DBRepository.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO employee(id, name, email, salary) " +
+                "VALUES(?, ?, ?, ?)");
+        preparedStatement.setInt(1, employee.getId());
+        preparedStatement.setString(2, employee.getName());
+        preparedStatement.setString(3, employee.getEmail());
+        preparedStatement.setFloat(4, employee.getSalary());
+
+        int x = preparedStatement.executeUpdate();
+        if (x > 0){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 }
